de6d93f71fa8fbb30d7b6d4b61ac4eeb80092593
yadhu <yadhucb96@gmail.com>
2022-07-14 15:41:33 +0530

    Done mail configuration in settings.py

--- appleshop/settings.py
+++ appleshop/settings.py
@@ -158,4 +158,11 @@ from django.contrib.messages import constants as messages
 
 MESSAGE_TAGS = {messages.ERROR: "danger"}
 
+#mail configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS =True
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
